54ed299e3377a0301748d9366ebeafcd4894a57c
Vikas Singh <Vikas@olibr.com>
2025-12-18 17:14:39 +0530

    Add Vercel Analytics and multiple images to Dhurandhar blog post

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins, Ubuntu } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -44,6 +45,7 @@ export default function RootLayout({
         className={`${poppins.variable} ${ubuntu.variable} font-sans antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   )
