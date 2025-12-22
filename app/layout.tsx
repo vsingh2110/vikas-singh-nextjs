53c9aeed18fd91426da494496c822e4def570f86
Vikas Singh <Vikas@olibr.com>
2025-12-22 16:15:52 +0530

    feat: Integrate Google Tag Manager for enhanced tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins, Ubuntu } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -44,6 +45,33 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} ${ubuntu.variable} font-sans antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KWKQK668"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
+        {/* Google Tag Manager */}
+        <Script 
+          id="google-tag-manager" 
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KWKQK668');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {children}
         <Analytics />
       </body>
